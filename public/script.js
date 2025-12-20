349bf89e155da72e20d720ae29a250b86aa914b3
Vida <quiniavidamarie30@gmail.com>
2025-12-20 22:55:23 +0800

    added a way to view selected ships hover in ship selection area

--- public/script.js
+++ public/script.js
@@ -1,6 +1,8 @@
 ( function() {
     openShipPlacement()
     renderingBoardCells()
+    toggleOrientationButton()
+    placeShip()
 })()
 
 function renderingBoardCells(){
@@ -24,9 +26,15 @@ function renderingBoardCells(){
                     cell.classList.add('game-header')
                 }
 
-                if ( i === 0 && j === 0 ) cell.classList.add('game-header')
-                grid.appendChild(cell)
+                if ( i !== 0 && j !== 0 ) {
+                    cell.dataset.row = i - 1
+                    cell.dataset.col = j - 1
+                    cell.classList.add('cell')
+                } 
 
+                if ( i === 0 && j === 0 ) cell.classList.add('game-header')
+                
+                grid.appendChild(cell)
             }
         }
     })
@@ -44,4 +52,72 @@ function openShipPlacement(){
     const shipPlacementScreen = document.querySelector('.ship-placement')
     mainGameScreen.style.display = 'none'
     shipPlacementScreen.style.display = 'flex'
+}
+
+function toggleOrientationButton(){
+    const toggleButton = document.querySelector('.ship-orientation')
+    toggleButton.addEventListener( 'click' , () => {
+        const orientation = toggleButton.getAttribute('data-orientation')
+        if( orientation === 'horizontal' ){
+            toggleButton.dataset.orientation = 'vertical'
+            toggleButton.textContent = 'Vertical'
+        }
+        else{
+            toggleButton.dataset.orientation = 'horizontal'
+            toggleButton.textContent = 'Horizontal'
+        }
+    })
+}
+
+function placeShip(){
+    const currentGrid = document.querySelector('.place-ships')
+    const cells = currentGrid.querySelectorAll('.cell')
+    const toggleButton = document.querySelector('.ship-orientation')
+    cells.forEach( ( cell ) => {
+        cell.addEventListener( 'mouseenter' , () => {
+            const colCords = Number(cell.getAttribute('data-col'))
+            const rowCords = Number(cell.getAttribute('data-row'))
+            const orientation = (toggleButton.getAttribute('data-orientation'))
+
+            if(orientation === 'vertical'){
+                let selectedCells = []
+                
+                for(let i = 0 ; i < 5 ; i++ ){
+                    const newCells = getCell( rowCords + i , colCords , currentGrid )
+                    selectedCells.push( newCells )
+                }
+
+                selectedCells.forEach( ( selectedCell ) => {
+                    selectedCell.classList.add('selected')
+                })
+            }
+
+            else{
+                let selectedCells = []
+                
+                for(let i = 0 ; i < 5 ; i++ ){
+                    const newCells = getCell( rowCords , colCords + i , currentGrid )
+                    selectedCells.push( newCells )
+                }
+
+                console.log(selectedCells)
+
+                selectedCells.forEach( ( selectedCell ) => {
+                    selectedCell.classList.add('selected')
+                })
+            }
+
+        })
+
+        cell.addEventListener( 'mouseleave' , () => {
+            cells.forEach((selectedCell) => {
+                selectedCell.classList.remove('selected')
+            })
+            
+        })
+    })
+}
+
+function getCell( row , col , grid){
+    return  grid.querySelector(`.cell[data-row="${row}"][data-col="${col}"]`)
 }
